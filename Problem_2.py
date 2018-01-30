8fb6dac95083e968d245c13476c10dc626a56b4a
Javier <jarechalde@gmail.com>
2018-01-30 14:56:42 -0500

    Add files via upload

---
+++ Problem_2.py
@@ -0,0 +1,28 @@
+men = ['V','W','X','Y','Z']
+women = ['A','B','C','D','E']
+
+#Men's preferences
+vpref = ['B','A','D','E','C']
+wpref = ['D','B','A','C','E']
+xpref = ['B','E','C','D','A']
+ypref = ['A','D','C','B','E']
+zpref = ['B','D','A','E','C']
+
+#Woman's preferences
+apref = ['Z','V','W','Y','X']
+bpref = ['X','W','Y','V','Z']
+cpref = ['W','X','Y','Z','V']
+dpref = ['V','Z','Y','X','W']
+epref = ['Y','W','Z','X','V']
+
+#Implementation of the propose and reject algorithm
+
+
+###Changing the man order of proposal###
+
+#In this case the order of man proposing goes from Z to V instead
+
+###Another different implementation###
+
+#What if women were proposing to men instead?
+
