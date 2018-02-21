523c7def9b0fc5450f8e636db1e310ddfd6f36eb
jarechalde <jarechalde@gmail.com>
2018-02-20 20:45:05 -0500

    First implementation of the scheduling algorithm

---
+++ Assignment_3/Schedule.py
@@ -0,0 +1,7 @@
+#We will first create a  list that contains the participants in the competition
+#and their swimming time, biking time, and running time
+contestants = [
+(1,2,3), #Contestant #1
+(2,3,4), #...
+(4,5,3)  #Contestant #N
+]
