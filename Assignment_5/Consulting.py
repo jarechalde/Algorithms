00bd52bbf033edda3e03e8b57487b56cf4abd2c0
jarechalde <jarechalde@gmail.com>
2018-03-30 11:59:09 -0400

    Almost final version

--- Assignment_5/Consulting.py
+++ Assignment_5/Consulting.py
@@ -1,48 +1,22 @@
 #Low stress jobs
-l = [10,1,80,10,10]
+l = [10,1,10,10]
 
 #High Stress jobs
-h = [5,50,160,5,1]
+h = [5,50,5,1]
 
 length = len(l)
 
 #Initializing the revenue to 0
-rev = 0
+rev = [0]*(length+1)
 
-#Adding 0 to the array
-l.append(0)
-h.append(0)
+#Initializing some values
+rev[0] = 0
+rev[1] = max(l[0],h[0])
 
-#Rest and skip variable
-rest = 0
-skip = 0
-
-for i in range(0,length):
- 
- if skip==1:
-  skip = 0
-  continue
- 
- #If we chose to rest, we skip this iteration
- if rest == 1:
-  if h[i+1]>h[i]+l[i+1]:
-   rev = rev+h[i+1]+l[i-1]
-   rest = 0
-   skip = 1
-   continue
-  else:
-   rev = rev+h[i]
-   rest = 0
-  continue
-
- print(i)
- print(l[i],l[i+1])
- if h[i+1]>(l[i]+l[i+1]):
-  print('Resting and choosing the high stress job')
-  rest = 1
- #Else we choose the low stress job
+for i in range(1,length):
+ p = i+1
+ if h[i]>l[i]+l[i-1]:
+  rev[p] = h[i]+rev[p-2]
  else:
-  print("Choosing the low stress job")
-  rev = rev+l[i]
-  
+  rev[p] = l[i]+rev[p-1]
 print(rev)
